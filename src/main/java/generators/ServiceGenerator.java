2ba6f0c9373b247752e5982297205395b77e2fe6
Patrick O'Connor <patrick.oconnor@workiva.com>
2018-06-25 20:33:57 -0700

    master: Add service annotations

--- src/main/java/generators/ServiceGenerator.java
+++ src/main/java/generators/ServiceGenerator.java
@@ -1,5 +1,6 @@
 package generators;
 
+import io.swagger.models.HttpMethod;
 import io.swagger.models.Operation;
 import io.swagger.models.Path;
 
@@ -10,8 +11,8 @@ public class ServiceGenerator {
     public static String generate(String title, Map<String, Path> paths) {
         StringBuilder builder = new StringBuilder();
         builder.append(generateHeader(title));
-        paths.forEach((String name, Path path) -> path.getOperations()
-                .forEach((Operation o) -> builder.append(generateMethod(o.getOperationId()))));
+        paths.forEach((String name, Path path) -> path.getOperationMap()
+                .forEach((HttpMethod method, Operation o) -> builder.append(generateMethod(o.getOperationId(), name, method.name()))));
         builder.append(generateFooter());
         return builder.toString();
     }
@@ -20,14 +21,20 @@ public class ServiceGenerator {
         return "}\n";
     }
 
-    private static String generateMethod(String operationId) {
-        return String.format("    %sResponse %s(1: %sRequest request)\n",
+    private static String generateMethod(String operationId, String path, String method) {
+        return String.format("    %sResponse %s(1: %sRequest request) (http.pathTemplate=\"%s\") (http.method=\"%s\")\n",
+                             capitalize(operationId),
                              operationId,
-                             operationId,
-                             operationId);
+                             capitalize(operationId),
+                             path,
+                             method);
     }
 
     private static String generateHeader(String title) {
         return String.format("service %sService {\n", title);
+    }
+
+    private static String capitalize(String in) {
+        return in.substring(0, 1).toUpperCase() + in.substring(1);
     }
 }
